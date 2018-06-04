c4fb936b9c15671fd109c258093ef4f972dede37
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-06-04 14:53:28 -0500

    Added database object, functions to save and load

---
+++ main.js
@@ -0,0 +1,52 @@
+const HomeInventoryDatabase = {};
+
+HomeInventoryDatabase.furniture = [];
+HomeInventoryDatabase.electronics = [];
+HomeInventoryDatabase.stuff = [];
+
+const ottoman = {
+  name: "The Ottoman Emperor",
+  location: "Living room",
+  description: "This ottoman is basically the best ottoman ever: it is large and soft, looks nice, and can be use for so many things"
+}
+HomeInventoryDatabase.furniture.push(ottoman);
+
+const television = {
+  name: "Old television",
+  location: "Living room",
+  description: "Whenever you turn this old piece of junk on, it will keep trying to turn itself off again until you make it clear that you aren't going to give up"
+}
+HomeInventoryDatabase.electronics.push(television);
+
+const fishbowl = {
+  name: "Fido's fishbowl",
+  location: "Living room",
+  description: "This sleek, modern fishbowl is the home of my betta fish, Fido"
+}
+HomeInventoryDatabase.stuff.push(fishbowl);
+
+const desktop = {
+  name: "Rachel's computer",
+  location: "Bonus room",
+  description: "Once a useful device for mooching Netflix off of Rachel's old roommate, this desktop computer is now primarily used when Rachel is on call for work or editing photos"
+}
+HomeInventoryDatabase.electronics.push(desktop);
+
+const table = {
+  name: "Family heirloom table",
+  location: "Dining room",
+  description: "This table has been in the family for three generations. It is very old and is falling apart. Because it is very old, my mom acts like it will last forever. Because it is falling apart, I act like it won't"
+}
+HomeInventoryDatabase.furniture.push(table);
+
+const saveDatabase = (databaseObject, localStorageKey) => {
+  const stringifiedDatabase = JSON.stringify(databaseObject);
+  localStorage.setItem(localStorageKey, stringifiedDatabase);
+}
+
+const loadDatabase = (localStorageKey) => {
+  const databaseString = localStorage.getItem(localStorageKey);
+  return JSON.parse(databaseString);
+}
+
+saveDatabase(HomeInventoryDatabase, "HomeInventory");
