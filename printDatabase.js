5867b8c0b5ccdc0f780d8f5f5c36921b3e1af15e
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-06-04 16:05:51 -0500

    Refactored code to make it DRY

--- printDatabase.js
+++ printDatabase.js
@@ -1,5 +1,5 @@
-const loadDatabase = (localStorageKey) => {
-  const databaseString = localStorage.getItem(localStorageKey);
+const loadDatabase = (localStoragecategory) => {
+  const databaseString = localStorage.getItem(localStoragecategory);
   return JSON.parse(databaseString);
 }
 
@@ -9,40 +9,14 @@ const capitalizeFirst = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-const getAllThings = () => {
-  for (key in inventory) { // goes through each category (e.g. furniture)
-    const heading = document.createElement("h2");
-    heading.textContent = `${capitalizeFirst(key)}`;
-    fragment.appendChild(heading);
-
-    inventory[key].forEach(thing => { // goes through each thing in category
-      const section = document.createElement("section");
-      for (prop in thing) { // goes through all properties of a thing
-        const paragraph = document.createElement("p");
-        let placeholder = `${capitalizeFirst(prop)}: `
-        if (prop === "name") {
-          paragraph.style.fontWeight = "bold";
-          paragraph.style.textDecoration = "underline";
-          placeholder = "";
-        }
-        paragraph.textContent = `${placeholder}${thing[prop]}`;
-        section.appendChild(paragraph);
-      }
-      fragment.appendChild(section);
-    });
-  }
-  const article = document.querySelector("#myThings");
-  article.appendChild(fragment);
+const makeSectionHeading = (headText) => {
+  const heading = document.createElement("h2");
+  heading.textContent = `${capitalizeFirst(headText)}`;
+  fragment.appendChild(heading);
 }
 
-// getAllThings();
-
-const getSomeThings = (category) => {
-  const heading = document.createElement("h2");
-  heading.textContent = `${capitalizeFirst(category)}`;
-  fragment.appendChild(heading);
-
-  inventory[category].forEach(thing => { // goes through each thing in category
+const makeSection = (key) => {
+  inventory[key].forEach(thing => { // goes through each thing in category
     const section = document.createElement("section");
     for (prop in thing) { // goes through all properties of a thing
       const paragraph = document.createElement("p");
@@ -57,17 +31,34 @@ const getSomeThings = (category) => {
     }
     fragment.appendChild(section);
   });
+}
 
-const article = document.querySelector("#myThings");
-article.appendChild(fragment);
+const writeFragment = () => {
+  const article = document.querySelector("#myThings");
+  article.appendChild(fragment);
+}
+
+const getAllThings = () => {
+  for (category in inventory) { // goes through each category (e.g. furniture)
+    makeSectionHeading(category);
+    makeSection(category);
+  }
+  writeFragment();
+  
+}
+
+const getSomeThings = (category) => {
+  makeSectionHeading(category);
+  makeSection(category);
+  writeFragment();
 }
 
 const getThings = (parameter) => {
-  if (parameter === undefined) {
+  if (parameter === "") {
     getAllThings();
   } else {
     getSomeThings(parameter);
   }
 }
 
-getThings();
+getThings("furniture");
